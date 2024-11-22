24a8e1e9ab431e98ea9e553815315cc8b154bc08
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-11-22 23:10:04 +0200

    Added_Controllers_Models

---
+++ Lab6/Controllers/Ref_Contact_TypesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab6.Controllers
+{
+    public class Ref_Contact_TypesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
